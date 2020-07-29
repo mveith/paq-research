07a0e225397e5c1d66c21b662b78d715faeb94db
mveith <miroveith@gmail.com>
2020-07-29 23:11:36 +0200

    Logo

--- pages/index.js
+++ pages/index.js
@@ -52,12 +52,11 @@ export default function Home() {
 
       <footer className={styles.footer}>
         <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          href="https://www.paqresearch.cz/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+          <img src="/paqresearch.png" alt="PAQ Research Logo" className={styles.logo} />
         </a>
       </footer>
     </div>
