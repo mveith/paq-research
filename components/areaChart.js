ca4fca84c9494f6a94506cdf3142696235f0b9d0
mveith <miroveith@gmail.com>
2020-08-20 21:25:52 +0200

    AreaChart component

---
+++ components/areaChart.js
@@ -0,0 +1,85 @@
+import dynamic from 'next/dynamic'
+
+const Chart = dynamic(
+    () => import('../components/chart'),
+    { ssr: false }
+)
+
+function generateAnnotations(props) {
+    if (props.annotation) {
+        const values = props.values;
+        const tooltipAnnotations = values.lines.map((l, i) => {
+            return {
+                type: "frame-hover",
+                x: props.annotation.week,
+                y: values.lines.slice(i).map(pl => pl[props.annotation.week - 1]).reduce((a, b) => a + b, 0),
+                value: l[props.annotation.week - 1]
+            };
+        });
+        return [{ type: "x", week: props.annotation.week, disable: ["connector", "note"] }].concat(tooltipAnnotations);
+
+    }
+    else return [];
+}
+
+function AreaChart(props) {
+    const values = props.values;
+    const lines = values.lines.map((l, li) => { return { coordinates: l.map((v, i) => { return { week: i + 1, value: values.lines.slice(li).map(pl => pl[i]).reduce((a, b) => a + b, 0) }; }) }; });
+
+    const annotations = generateAnnotations(props);
+
+    const yAxis = props.showYAxis ?
+        { orient: "left", tickValues: [0, 100], baseline: false, showOutboundTickLines: false, tickLineGenerator: e => null, tickFormat: function (e) { return e + "%" } } :
+        { orient: "left", ticks: 0, baseline: false, showOutboundTickLines: false, tickLineGenerator: e => null, tickFormat: e => null };
+
+    const xAxis = {
+        orient: "bottom", showOutboundTickLines: true, ticks: props.weeks, tickFormat: (e => props.showXAxis ? `${e}.${e === 1 ? "vlna" : ""}` : null), tickLineGenerator: ({ xy }) => (
+            <line
+                key={`line-${xy.y1}-${xy.x1}`}
+                x1={xy.x1}
+                x2={xy.x2}
+                y1={xy.y1}
+                y2={xy.y2}
+                style={{
+                    strokeDasharray: "3 3",
+                    stroke: "white",
+                    strokeOpacity: 0.4
+                }}
+            />
+        )
+    };
+
+    const frameProps = {
+        lines: lines,
+        size: props.size,
+        margin: { left: 80, bottom: props.showXAxis ? 50 : 10, right: 10, top: 40 },
+
+        lineType: "area",
+        responsiveWidth: true,
+        xAccessor: "week",
+        yAccessor: "value",
+        yExtent: [props.yMin, props.yMax],
+        lineDataAccessor: "coordinates",
+
+        lineStyle: (d, i) => ({
+            fill: props.colors[i],
+            fillOpacity: 1
+        }),
+        pointStyle: { fill: "none", stroke: "gray", strokeWidth: "1px" },
+        title: (
+            <text textAnchor="middle">
+                {values.title}
+            </text>
+        ),
+        axes: [yAxis, xAxis],
+        hoverAnnotation: [
+            { type: "x", disable: ["connector", "note"] },
+        ],
+        annotations: annotations,
+        customHoverBehavior: x => props.onHover ? props.onHover(x) : null,
+        tooltipContent: d => { return <div style={{ margin: "5px" }}>{d.value} %</div> }
+    };
+    return <Chart {...frameProps} />;
+}
+
+export default AreaChart;
--- pages/dopad.js
+++ pages/dopad.js
@@ -1,31 +1,9 @@
 import fs from 'fs'
 import path from 'path'
-import dynamic from 'next/dynamic'
 import Layout from '../components/layout';
+import AreaChart from '../components/areaChart';
 import { useState } from 'react';
 import Link from 'next/link';
-
-const Chart = dynamic(
-    () => import('../components/chart'),
-    { ssr: false }
-)
-
-function generateAnnotations(props) {
-    if (props.annotation) {
-        const values = props.values;
-        const tooltipAnnotations = values.lines.map((l, i) => {
-            return {
-                type: "frame-hover",
-                x: props.annotation.week,
-                y: values.lines.slice(i).map(pl => pl[props.annotation.week - 1]).reduce((a, b) => a + b, 0),
-                value: l[props.annotation.week - 1]
-            };
-        });
-        return [{ type: "x", week: props.annotation.week, disable: ["connector", "note"] }].concat(tooltipAnnotations);
-
-    }
-    else return [];
-}
 
 function ThemeNavigation() {
     return (
@@ -51,78 +29,18 @@ function ThemeNavigation() {
         </div>);
 }
 
-function ImpactChart(props) {
-    const values = props.values;
-    const lines = values.lines.map((l, li) => { return { coordinates: l.map((v, i) => { return { week: i + 1, value: values.lines.slice(li).map(pl => pl[i]).reduce((a, b) => a + b, 0) }; }) }; });
-
-    const annotations = generateAnnotations(props);
-
-    const yAxis = props.showYAxis ?
-        { orient: "left", tickValues: [0, 100], baseline: false, showOutboundTickLines: false, tickLineGenerator: e => null, tickFormat: function (e) { return e + "%" } } :
-        { orient: "left", ticks: 0, baseline: false, showOutboundTickLines: false, tickLineGenerator: e => null, tickFormat: e => null };
-
-    const xAxis = {
-        orient: "bottom", showOutboundTickLines: true, ticks: props.weeks, tickFormat: (e => props.showXAxis ? `${e}.${e === 1 ? "vlna" : ""}` : null), tickLineGenerator: ({ xy }) => (
-            <line
-                key={`line-${xy.y1}-${xy.x1}`}
-                x1={xy.x1}
-                x2={xy.x2}
-                y1={xy.y1}
-                y2={xy.y2}
-                style={{
-                    strokeDasharray: "3 3",
-                    stroke: "white",
-                    strokeOpacity: 0.4
-                }}
-            />
-        )
-    };
-
-    const frameProps = {
-        lines: lines,
-        size: props.size,
-        margin: { left: 80, bottom: props.showXAxis ? 50 : 10, right: 10, top: 40 },
-
-        lineType: "area",
-        responsiveWidth: true,
-        xAccessor: "week",
-        yAccessor: "value",
-        yExtent: [props.yMin, props.yMax],
-        lineDataAccessor: "coordinates",
-
-        lineStyle: (d, i) => ({
-            fill: props.colors[i],
-            fillOpacity: 1
-        }),
-        pointStyle: { fill: "none", stroke: "gray", strokeWidth: "1px" },
-        title: (
-            <text textAnchor="middle">
-                {values.title}
-            </text>
-        ),
-        axes: [yAxis, xAxis],
-        hoverAnnotation: [
-            { type: "x", disable: ["connector", "note"] },
-        ],
-        annotations: annotations,
-        customHoverBehavior: x => props.onHover ? props.onHover(x) : null,
-        tooltipContent: d => { return <div style={{ margin: "5px" }}>{d.value} %</div> }
-    };
-    return <Chart {...frameProps} />;
-}
-
 export default function Impact(props) {
     const [annotation, setAnnotation] = useState();
     const [total, setTotal] = useState(true);
     const charts = props.groups.map((v, i) => {
-        return (<ImpactChart key={`impact-chart-${i}`} weeks={props.weeks} colors={props.colors} titles={props.titles} yMin={0} yMax={100} showYAxis={i % 3 === 0} showXAxis={false} values={v} size={[300, 200]} annotation={annotation} onHover={x => {
+        return (<AreaChart key={`impact-chart-${i}`} weeks={props.weeks} colors={props.colors} titles={props.titles} yMin={0} yMax={100} showYAxis={i % 3 === 0} showXAxis={false} values={v} size={[300, 200]} annotation={annotation} onHover={x => {
             if (x) {
                 setAnnotation({ week: x.week, lineIndex: x.parentLine.key });
             }
             else { setAnnotation(); }
         }} />);
     });
-    const totalChart = (<ImpactChart key="impact-chart-total" weeks={props.weeks} colors={props.colors} titles={props.titles} yMin={0} yMax={100} showYAxis={true} showXAxis={true} values={props.total} size={[800, 600]} annotation={annotation} onHover={x => {
+    const totalChart = (<AreaChart key="impact-chart-total" weeks={props.weeks} colors={props.colors} titles={props.titles} yMin={0} yMax={100} showYAxis={true} showXAxis={true} values={props.total} size={[800, 600]} annotation={annotation} onHover={x => {
         if (x) {
             setAnnotation({ week: x.week, lineIndex: x.parentLine.key });
         }
