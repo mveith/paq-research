359f6f6e722342e9f55b3f46b1202ce27b2b6d26
mveith <miroveith@gmail.com>
2020-08-30 21:39:09 +0200

    Vertical grid lines (20%) + small refactoring

--- components/areaChart.js
+++ components/areaChart.js
@@ -22,32 +22,59 @@ function generateAnnotations(props) {
     else return [];
 }
 
+function TickLine({ xy }) {
+    return (<line
+        key={`line-${xy.y1}-${xy.x1}`}
+        x1={xy.x1}
+        x2={xy.x2}
+        y1={xy.y1}
+        y2={xy.y2}
+        style={{
+            strokeDasharray: "3 3",
+            stroke: "white",
+            strokeOpacity: 0.4
+        }}
+    />)
+}
+
+function getYAxis(props) {
+    const yAxisTicks = Array.from(new Array(5 + 1), (x, i) => i * 20);
+    if (!props.showYAxis) {
+        return { orient: "left", ticks: 0, baseline: false, showOutboundTickLines: false, tickLineGenerator: e => null, tickFormat: e => null };
+    }
+
+    const getTickValue = e => {
+        if (e === 0 || e === 100) {
+            return e + "%";
+        }
+        return null;
+    };
+    return {
+        orient: "left",
+        tickValues: yAxisTicks,
+        baseline: false,
+        showOutboundTickLines: false,
+        tickLineGenerator: ({ xy }) => (<TickLine xy={xy} />),
+        tickFormat: getTickValue
+    };
+}
+
+function getXAxis(props) {
+    const getTickValue = e => props.showXAxis ? `${e}.${e === props.firstWeek ? "vlna" : ""}` : null;
+    return {
+        orient: "bottom",
+        showOutboundTickLines: true,
+        ticks: props.weeks,
+        tickFormat: getTickValue,
+        tickLineGenerator: ({ xy }) => (<TickLine xy={xy} />)
+    };
+}
+
 function AreaChart(props) {
     const values = props.values;
     const lines = values.lines.map((l, li) => { return { coordinates: l.map((v, i) => { return { week: i + props.firstWeek, value: values.lines.slice(li).map(pl => pl[i]).reduce((a, b) => a + b, 0) }; }) }; });
 
     const annotations = generateAnnotations(props);
-
-    const yAxis = props.showYAxis ?
-        { orient: "left", tickValues: [0, 100], baseline: false, showOutboundTickLines: false, tickLineGenerator: e => null, tickFormat: function (e) { return e + "%" } } :
-        { orient: "left", ticks: 0, baseline: false, showOutboundTickLines: false, tickLineGenerator: e => null, tickFormat: e => null };
-
-    const xAxis = {
-        orient: "bottom", showOutboundTickLines: true, ticks: props.weeks, tickFormat: (e => props.showXAxis ? `${e}.${e === props.firstWeek ? "vlna" : ""}` : null), tickLineGenerator: ({ xy }) => (
-            <line
-                key={`line-${xy.y1}-${xy.x1}`}
-                x1={xy.x1}
-                x2={xy.x2}
-                y1={xy.y1}
-                y2={xy.y2}
-                style={{
-                    strokeDasharray: "3 3",
-                    stroke: "white",
-                    strokeOpacity: 0.4
-                }}
-            />
-        )
-    };
 
     const frameProps = {
         lines: lines,
@@ -71,7 +98,7 @@ function AreaChart(props) {
                 {values.title}
             </text>
         ),
-        axes: [yAxis, xAxis],
+        axes: [getYAxis(props), getXAxis(props)],
         hoverAnnotation: [
             { type: "x", disable: ["connector", "note"] },
         ],
