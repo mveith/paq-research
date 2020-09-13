3722e9fac1dae0800967161da2b921e19ed7c738
mveith <miroveith@gmail.com>
2020-09-13 19:45:37 +0200

    Fix

--- pages/protektivni-aktivity.js
+++ pages/protektivni-aktivity.js
@@ -10,7 +10,7 @@ export default function Protection(props) {
         navigation={navigation}
         dataProps={props}
         title="Jak časté jsou různé formy ochrany před nákazou?"
-        description="Ostražitost populace se v průběhu posledních měsíců výrazně mění. Během jarní vlny epidemie dělaly dvě pětiny respondentů téměř všechny zkoumané protektivní aktivity (nošení roušky, pečlivé mytí rukou, omezování fyzických kontaktů a pobytu na přelidněných místech, atd.). Dnes to je jen okolo sedminy respondentů. Naopak více než polovina přijímá v současnosti menšinu těchto ochranných opatření."
+        description="Ostražitost populace se v průběhu posledních měsíců výrazně mění. Během jarní vlny epidemie dělaly dvě pětiny respondentů téměř všechny zkoumané protektivní aktivity (nošení roušky, pečlivé mytí rukou, omezování fyzických kontaktů a pobytu na přelidněných místech, atd.). Dnes to je jen okolo desetiny respondentů. Naopak více než polovina přijímá v současnosti menšinu těchto ochranných opatření."
         shareImage="protektivni-aktivity"
     />;
 }
