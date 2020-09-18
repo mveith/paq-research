879c2caa79169d7c8c586712dc81b00058acc4c3
mveith <miroveith@gmail.com>
2020-09-19 00:24:43 +0200

    Dynamic routes

--- components/dataProvider.js
+++ components/dataProvider.js
@@ -1,14 +1,14 @@
 import fs from 'fs'
 import path from 'path'
 
-export default async function getSourceData() {
+export default async function getSourceData(fileName) {
     var json;
-    if (process.env.PAQ_DATA_PATH) {
-        const res = await fetch(process.env.PAQ_DATA_PATH);
+    if (process.env.PAQ_DATA_PATH_TO_FILE) {
+        const res = await fetch(path.join(process.env.PAQ_DATA_PATH_TO_FILE, fileName));
         json = await res.text();
     }
     else {
-        const filePath = path.join(process.cwd(), 'sample/data.json');
+        const filePath = path.join(process.cwd(), 'sample', fileName);
         json = fs.readFileSync(filePath, 'utf8');
     }
 
---
+++ pages/[key].js
@@ -0,0 +1,29 @@
+import getSourceData from '../components/dataProvider'
+import DataPage from '../components/dataPage';
+import ThemeNavigation from '../components/themeNavigation';
+
+export default function Page(props) {
+    return <DataPage
+        navigation={<ThemeNavigation previousHref={props.previousHref} previousTitle={props.previousTitle} nextHref={props.nextHref} nextTitle={props.nextTitle} />}
+        dataProps={props.dataProps}
+    />;
+}
+
+export async function getStaticProps(context) {
+    const data = await getSourceData(`${context.params.key}.json`);
+    const structure = await getSourceData("structure.json");
+    const currentIndex = structure.pages.map(p => p.key).indexOf(context.params.key);
+    const previous = currentIndex > 0 ? structure.pages[currentIndex - 1] : structure.pages[structure.pages.length - 1];
+    const next = currentIndex < structure.pages.length - 1 ? structure.pages[currentIndex + 1] : structure.pages[0];
+    return {
+        props: { dataProps: data, previousHref: `/${previous.key}`, previousTitle: previous.title, nextHref: `/${next.key}`, nextTitle: next.title }
+    }
+}
+
+export async function getStaticPaths() {
+    const data = await getSourceData("structure.json");
+    return {
+        paths: data.pages.map(p => { return { params: { key: p.key } }; }),
+        fallback: false
+    };
+}
