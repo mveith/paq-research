c16f6123c9bcb91592b5fcaf50a1d97bb9f230d8
mveith <miroveith@gmail.com>
2020-10-07 21:47:36 +0200

    Small legend

--- components/legend.js
+++ components/legend.js
@@ -1,17 +1,19 @@
-function LegendItem({ color, title, description }) {
+function LegendItem({ color, title, description, smallItem }) {
+    const style = smallItem ? { fontSize:"0.9rem" } : {};
     return (<li>
-        <h2 className="legend-title" style={{ color: color }}>{title}</h2>
+        <h2 className="legend-title" style={{...style, color: color }}>{title}</h2>
         <p className="legend-description">{description}</p>
     </li>);
 }
 
 export default function Legend(props) {
     const title = props.title ? <h2 className="legend-title">{props.title}</h2> : <></>;
+    const smallLegend = props.items.length > 10;
     return (
         <>
             {title}
             <ul style={{ listStyle: "none", flexBasis: "20%", paddingLeft: "0" }}>
-                {props.items.map((item, i) => <LegendItem {...item} key={`legend-item-${i}`} />)}
+                {props.items.map((item, i) => <LegendItem {...item} key={`legend-item-${i}`} smallItem={smallLegend} />)}
             </ul>
         </>);
 }
